aca6371a0a2dba971abc804d66f3fa5a7b17eafc
Matthew Moorcroft <matthew.moorcroft@databricks.com>
2023-10-05 09:32:10 +0100

    Bundle Demo

--- taxi_bronze.py
+++ taxi_bronze.py
@@ -5,14 +5,13 @@ from pyspark.sql import DataFrame
 from pyspark.sql.functions import regexp_replace
 
 
-@dlt.table
-@dlt.expect("No null links", "link is not null")
-def medium_raw():
-    file_location = "/FileStore/tables/yellow_tripdata_2023_01.parquet"
-    file_type = "parquet"
+file_location = "/FileStore/tables/yellow_tripdata_2023_01.parquet"
+file_type = "parquet"
 
-    return spark.read.format(file_type).load(file_location)
+df =  spark.read.format(file_type).load(file_location)
 
+
+df.write.saveAsTable(f"")
 
 @dlt.table
 def medium_clean():
